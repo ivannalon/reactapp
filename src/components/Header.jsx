39118be4e8646d69f2f5fe01fee1b583656811c7
Ivan Nalon <ivannalon@gmail.com>
2022-06-06 10:13:50 -0300

    Finalizado o Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import styles from "./Header.module.css"
 
+import igniteLogo from "../assets/ignite-logo.svg"
+
 export function Header(){
   return (
-    <strong className={styles.header}>NFT Feed</strong>
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logo do Ignite" />
+      <strong>Ignite Feed</strong>
+    </header>
   )
 }
